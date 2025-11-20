07dd424d16a2fc5f7a7cbbfcf4546c9756a4330d
MrBigBrainy <mrbigbrainy@gmail.com>
2025-11-20 11:42:57 +0700

    start project server

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express from "express"
+import cors from "cors"
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(3000, () => console.log("SERVER IS STARTING AT PORT 3000"))
