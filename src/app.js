81a1e088f34c405766ebed75f1f0a52147fc6154
MrBigBrainy <mrbigbrainy@gmail.com>
2025-11-20 11:48:14 +0700

    start auth router

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from "express"
 import cors from "cors"
+import { authRouter } from "./router/auth.route";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("auth", authRouter)
 
 app.listen(3000, () => console.log("SERVER IS STARTING AT PORT 3000"))
