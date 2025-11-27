08863154bb97136daf81634e7ca3a2fc477a24d7
rattanasiri <rattanasiri10516@gmail.com>
2025-11-27 14:45:39 +0700

    try to clone

--- src/router/auth.route.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express"
-
-export const authRouter = Router();
-
-authRouter.post("/register", (req, res) => { res.send('register') })
-authRouter.post("/login", (req, res) => { res.send('login') })
-
-
--- src/router/store.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getStoreMenuController, getStoreCategoryController } from "../controller/store.controller.js";
-
-const storeRouter = Router();
-
-storeRouter.get("/:storeId/menu", getStoreMenuController);
-storeRouter.get("/:storeId/category", getStoreCategoryController);
-
-export default storeRouter;
