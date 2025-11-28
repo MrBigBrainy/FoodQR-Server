d4dfd22e01d0f37cd7c6571773215745e79bc6ae
rattanasiri <rattanasiri10516@gmail.com>
2025-11-28 15:42:42 +0700

    update createPool database

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,13 +1,14 @@
-import dotenv from 'dotenv';
-import { defineConfig } from 'drizzle-kit';
+import dotenv from "dotenv";
+import { defineConfig } from "drizzle-kit";
 
 dotenv.config();
 
 export default defineConfig({
-    out: './drizzle',
-    schema: './drizzle/schema.js',
-    dialect: 'mysql',
-    dbCredentials: {
-        url: process.env.DATABASE_URL,
-    },
+  out: "./drizzle",
+  schema: "./drizzle/schema.js",
+  dialect: "mysql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+    connectTimeout: 100000,
+  },
 });
