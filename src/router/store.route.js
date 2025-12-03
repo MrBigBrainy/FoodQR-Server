e2d5acc3bdc95a33ce2f2ee169f09880fc2b8601
racharnon2574-web <racharnon2574@gmail.com>
2025-12-03 14:02:03 +0700

    create Menu/backend

--- src/router/store.route.js
+++ src/router/store.route.js
@@ -3,10 +3,12 @@ import {
   getStoreMenuController,
   getStoreCategoryController,
 } from "../controller/store.controller.js";
+import { createMenu } from "../controller/menu.controller.js";
 
 const storeRouter = Router();
 storeRouter.get("/", (req, res) => res.send("TEST OK"));
 storeRouter.get("/:storeId/menu", getStoreMenuController);
 storeRouter.get("/:storeId/category", getStoreCategoryController);
+storeRouter.post("/menu", createMenu);
 
 export default storeRouter;
