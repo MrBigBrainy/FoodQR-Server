b16b782eaad32223450727f04018c92c22521c0f
rattanasiri <rattanasiri10516@gmail.com>
2025-12-09 10:59:04 +0700

    update

--- src/controller/menu.controller.js
+++ src/controller/menu.controller.js
@@ -1,11 +1,36 @@
-import { insertMenus } from "../services/menu.service.js";
+import { insertMenus, updateMenus } from "../services/menu.service.js";
 
 export const createMenu = async (req, res) => {
-    try {
-        const result = await insertMenus(req.body)
-        res.status(201).json({ message: "✅ เพิ่มเมนูสำเร็จ!" });
-    } catch (err) {
-        console.error("❌ createMenu error:", err);
-        res.status(500).json({ message: "เกิดข้อผิดดพลาดในการเพิ่มข้อมูล" });
-    }
+  const storeId = req.user?.storeId || req.body.storeId;
+
+  if (!storeId) {
+    return res
+      .status(403)
+      .json({ message: "Authentication is required to perform this action." });
+  }
+  try {
+    const result = await insertMenus(req.body);
+    res.status(201).json({ message: "✅ เพิ่มเมนูสำเร็จ!" });
+  } catch (err) {
+    console.error("❌ createMenu error:", err);
+    res.status(500).json({ message: "เกิดข้อผิดดพลาดในการเพิ่มข้อมูล" });
+  }
+};
+
+export const updateMenu = async (req, res) => {};
+const storeId = req.user?.storeId || req.body.storeId;
+
+if (!storeId) {
+  return res
+    .status(403)
+    .json({ message: "Authentication is required to perform this action." });
 }
+try {
+  const result = await updateMenu(req.body);
+  res.status(201).json({ message: "✅ เพิ่มเมนูสำเร็จ!" });
+} catch (err) {
+  console.error("❌ createMenu error:", err);
+  res.status(500).json({ message: "เกิดข้อผิดดพลาดในการเพิ่มข้อมูล" });
+}
+
+export const deleteMenu = async (req, res) => {};
